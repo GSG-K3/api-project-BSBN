fbcf80cd819f74d34ac44c0ca13f69632f59a46c
Sahar <sa.froukh92@hotmail.com>
2020-02-05 11:02:07 +0200

    add translation code #5

--- logic.js
+++ logic.js
@@ -1,13 +1,38 @@
-let songName = document.getElementById("searchSong").value ;
+let inputserch = document.getElementById("searchSong").value;
+let lyrics = document.getElementById("lyrics").innerText;
+let trnsLyrics= document.getElementById("transLyrics");
+let trnsBtn = document.getElementById("trnsBtn");
+
+let transUrl="https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200204T072713Z.2ce9652c3cfe1149.bba992d9ee381e66e45f3f03b186abd56134aa36&text="+lyrics+"&lang=en-ar";
 
 
 
-
-let xhr = new XMLHttpRequest();
-
+let request = function (recall, url){
+var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = ()=>{
-    if ( xhr.readyState === 4 && xhr.status === 200 ) {
-        let response = JSON.parse(xhr.responseText);
-        
+        if ( xhr.readyState === 4 && xhr.status === 200 ) {
+             let response = JSON.parse(xhr.responseText);
+             console.log(response);
+            // transLyrics.innerText = response.text;
+            if(recall) recall(response);
+            
+
+    
+        }
     }
+ 
+  xhr.open("GET", url);
+  xhr.send();
 }
+
+let translate = (response) => {
+    transLyrics.innerText = response.text;
+}
+
+trnsBtn.addEventListener("click",()=>{
+    request(translate,transUrl)
+});
+
+//request(translate,transUrl);
+
+
